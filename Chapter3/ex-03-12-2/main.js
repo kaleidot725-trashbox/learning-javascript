87e91a2f97f2f4110d4dc9446dfd4ef3f1fb33b7
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-04-12 22:21:22 +0900

    Update Chapter3

--- Chapter3/ex-03-12-2/main.js
+++ Chapter3/ex-03-12-2/main.js
@@ -1,3 +1,11 @@
-const s = "hello";
-s.rating = 3;
-console.log(s.rating);
+const a = ['a', 'b', 'c'];
+console.log(a.lenngth);
+console.log(a[0]);
+console.log(a[a.length - 1]);
+const b4 = [
+    { 名前: "ルビー", 硬度: 9 },
+    { 名前: "ダイヤ", 硬度: 10 },
+    { 名前: "トパーズ", 硬度: 8 },
+];
+console.log(b4[1].名前);
+console.log(b4[2].硬度);
