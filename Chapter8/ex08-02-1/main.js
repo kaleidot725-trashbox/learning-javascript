35c1d6bcd616c2a136b30030fefe7635471ad214
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-05-11 20:52:21 +0900

    Add Chapter8

---
+++ Chapter8/ex08-02-1/main.js
@@ -0,0 +1,9 @@
+let arr = ["b", "c", "d"];
+console.log(arr.push("e"));
+console.log(arr);
+console.log(arr.pop());
+console.log(arr);
+console.log(arr.unshift("a"));
+console.log(arr);
+console.log(arr.shift());
+console.log(arr);
---
+++ Chapter8/ex08-02-5/main.js
@@ -0,0 +1,7 @@
+let arr = [11, 12, 13, 14];
+let arr2 = arr.copyWithin(1, 2);
+console.log(arr);
+console.log(arr2);
+console.log(arr.copyWithin(2, 0, 2));
+console.log(arr.copyWithin(0, -3, -1));
+console.log(arr2);
