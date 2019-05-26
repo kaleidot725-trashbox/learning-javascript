937d977a2cbf77c51fdc1f5a18b6ec5e1d1f3c7b
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-05-26 11:25:45 +0900

    Update Chapter9

--- Chapter9/ex09-02-8/main.js
+++ Chapter9/ex09-02-8/main.js
@@ -39,5 +39,5 @@ const m = new Motorcycle();
 console.log(c instanceof Car);
 console.log(c instanceof Vehicle);
 console.log(m instanceof Car);
-console.log(m instanceof Vehicle);
 console.log(m instanceof Motorcycle);
+console.log(m instanceof Vehicle);
