901cd7b08a3cdbc3705ebd0475376653c62bab13
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-06-13 23:04:40 +0900

    Add Chapter18

---
+++ Chapter18/ex18-02-6-1/main.js
@@ -0,0 +1,13 @@
+const p1 = document.createElement('p');
+const p2 = document.createElement('p');
+
+p1.textContent = "この段落は動的に生成されました。";
+p2.textContent = "この段落は動的に生成された2つ目の段落です。";
+
+const parent = document.getElementById('content');
+const firstChild = parent.childNodes[0];
+alert("変更します。第一段階");
+parent.insertBefore(p1, firstChild);
+
+alert("変更します。第二段階");
+parent.appendChild(p2);
